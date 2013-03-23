8f138bb4c79af71df795da0928d26c46b9cef892
phpusr <phpusr@gmail.com>
2013-03-23 17:44:06 +0600

    Сделана генерация и вывод Рандомного массива

--- lab1/Hash.java
+++ lab1/Hash.java
@@ -6,6 +6,9 @@
  * To change this template use File | Settings | File Templates.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 15 вариант
  * Построить хеш - таблицу, содержащую последовательность из m=45 элементов размерности n=2. Элементы генерируются с помощью датчика случ. чисел.
@@ -13,5 +16,51 @@
  * Метод разрешения конфликта - квадратичные пробы.
  */
 public class Hash {
+    /** TODO */
+    private int num;
+    private boolean showHashEl;
+    private List<Integer> array;
 
+    /** Конструктор */
+    public Hash(int num, boolean showHashEl, List<Integer> array) {
+        this.num = num;
+        this.showHashEl = showHashEl;
+        this.array = array != null ? array : new ArrayList<Integer>();
+    }
+
+    /** Рандомная генерация массива */
+    public void generateRandomArray() {
+        if (array.size() == 0) {
+            for(int i=0; i<num; i++) {
+                array.add(getRandUniq());
+            }
+        }
+
+        printRandomArray();
+    }
+
+    /** Возвращает рандомное уникальное значение, размерности = 2 */
+    private int getRandUniq() {
+        int value = Math.round((float)Math.random()*90) + 10;
+        if (array.contains(value)) {
+            return getRandUniq();
+        } else {
+            return value;
+        }
+    }
+
+    /** Вывод рандомного массива */
+    private void printRandomArray() {
+        System.out.println("Random array (size: " + array.size() + "):");
+        for(Integer el : array) {
+            System.out.print(el + ", ");
+        }
+        System.out.println();
+    }
+
+    /** Главная функция */
+    public static void main(String[] args) {
+        Hash hash = new Hash(47, false, null);
+        hash.generateRandomArray();
+    }
 }
