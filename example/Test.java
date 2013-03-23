be88f5a2c8fe67bdff6a2c272d0e1043d71178f9
phpusr <phpusr@gmail.com>
2013-03-23 18:33:30 +0600

    Тестовый код

--- example/Test.java
+++ example/Test.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: phpusr
@@ -8,5 +10,23 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test");
+
+        List<Integer> hashArray = new ArrayList<Integer>();
+        for(int i=0; i<10; i++) {
+            hashArray.add(null);
+        }
+
+        hashArray.set(4, 5);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i + ": " + hashArray.get(i));
+        }
+
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        hashMap.put(49, 5);
+        hashMap.put(48, 5);
+
+        System.out.println(hashMap.keySet());
+        System.out.println(hashMap.toString());
     }
 }
