076ad35ba5dba9eeeaddd297cadd840dc572dc50
phpusr <phpusr@gmail.com>
2013-03-23 17:14:42 +0600

    Добавлен тестовый класс

---
+++ example/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: phpusr
+ * Date: 23.03.13
+ * Time: 17:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
