b46e5263d201a9bb08cba72ca686b0e1256a8a24
phpusr <phpusr@gmail.com>
2013-03-23 18:47:25 +0600

    Добавил функции для построения хеш-таблицы

--- lab1/Hash.java
+++ lab1/Hash.java
@@ -6,8 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 15 вариант
@@ -20,6 +19,9 @@ public class Hash {
     private int num;
     private boolean showHashEl;
     private List<Integer> array;
+    private int hashMapSize;
+    private Map<Integer, Integer> hashMap;
+    private List<Integer> probArray;
 
     /** Конструктор */
     public Hash(int num, boolean showHashEl, List<Integer> array) {
@@ -58,9 +60,62 @@ public class Hash {
         System.out.println();
     }
 
+    /** Построение хеш-таблицы */
+    public void buildHashTable() {
+        hashMapSize = Math.round((float)(array.size() * 1.5));
+        hashMap = new HashMap<Integer, Integer>();
+        probArray = new ArrayList<Integer>();
+
+        if (showHashEl) System.out.println("Hash elements:");
+        for (Integer el : array) {
+            probArray.add(putEl(el, 0));
+        }
+
+        printHashTable();
+    }
+
+    /** Помещение элемента в хеш-таблицу */
+    private int putEl(int el, int value) {
+        int index;
+        if (value == 0) {
+            index = hash(el) % hashMapSize;
+            if (showHashEl) System.out.println(el + "->" + index);
+        } else {
+            index = (hash(el) + value) % hashMapSize;
+        }
+
+        if (hashMap.get(index) == null) {
+            hashMap.put(index, el);
+            return value + 1;
+        } else {
+            return putEl(el, value+1);
+        }
+    }
+
+    /** Функция хеширования */
+    private int hash(int el) {
+        return Integer.parseInt(Integer.toString(el * el).substring(0, 1));
+    }
+
+    /** Вывод хеш-таблицы */
+    private void printHashTable() {
+        System.out.println("\nHash array (size: " + hashMapSize + "):");
+        for (int index=0; index<hashMapSize; index++) {
+            Integer el = hashMap.get(index);
+            String els = el != null ? el.toString() : " -";
+            System.out.print(index + "->" + els);
+            if (index < hashMapSize-1) System.out.print(",\t\t");
+            if ((index+1) % 5 == 0) System.out.println();
+        }
+
+        System.out.println();
+    }
+
     /** Главная функция */
     public static void main(String[] args) {
-        Hash hash = new Hash(47, false, null);
+        List<Integer> array = Arrays.asList(79,58,93,48,21);
+        Hash hash = new Hash(47, true, null);
         hash.generateRandomArray();
+        hash.buildHashTable();
     }
 }
